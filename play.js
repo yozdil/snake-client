2bb2318478d49a25edecc3c4d7525182b6e3e834
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-02-10 12:55:04 +0000

    client module

--- play.js
+++ play.js
@@ -1,22 +1,4 @@
-const net = require("net");
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  // data event handling
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
