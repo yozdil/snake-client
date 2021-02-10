310e356cac694b44035b640798cbe6b86ecc4774
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-02-10 13:31:26 +0000

    Stdin Moved to play.js

--- client.js
+++ client.js
@@ -36,35 +36,5 @@ const connect = function() {
   return conn;
 };
 
-/**
- * Setup User Interface
- * Specifically, so that we can handle user input via stdin
- */
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  // handleUserInput is the data callback as we want to exit the game using
-  // CTRL+C.
-  stdin.on('data', (key) => {
-    handleUserInput(key);
-  });
-  return stdin;
-};
-
-setupInput();
-
-
-// handleUserInput checks for the ctrl + c input and terminate the game. When
-// passed later into stdin.on data
-const handleUserInput = (key) => {
-  // \u0003 maps to ctrl+c input
-  if (key === "\u0003") {
-    process.exit();
-  }
-};
-
 
 module.exports = { connect };
--- play.js
+++ play.js
@@ -2,3 +2,34 @@ const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
+
+
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  // handleUserInput is the data callback as we want to exit the game using
+  // CTRL+C.
+  stdin.on('data', (key) => {
+    handleUserInput(key);
+  });
+  return stdin;
+};
+
+setupInput();
+
+
+// handleUserInput checks for the ctrl + c input and terminate the game. When
+// passed later into stdin.on data
+const handleUserInput = (key) => {
+  // \u0003 maps to ctrl+c input
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
